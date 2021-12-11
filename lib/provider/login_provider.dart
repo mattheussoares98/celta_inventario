f7750ca333fc79a80e787b426b5bd3889199be0b
mattheus <mattheussbarbosa@hotmail.com>
2021-12-11 08:31:55 -0300

    att

--- lib/provider/login_provider.dart
+++ lib/provider/login_provider.dart
@@ -30,27 +30,37 @@ class LoginProvider with ChangeNotifier {
     } else if (error ==
         '{Message: A senha está incorreta. Verifique a configuração do teclado e se a tecla [CAPS LOCK] está pressionada. Caso você tenha esquecido sua senha entre em contato com o administrador do seu sistema.}') {
       loginMessage = 'A senha está incorreta!';
+    } else if (error.contains('Connection timed')) {
+      loginMessage = 'Servidor não encontrado. Verifique a sua internet';
     } else {
       error = '';
       auth = true;
       loginMessage = '';
     }
+    auth = true;
     return '';
   }
 
   login([String user = '', String password = '']) async {
-    final response = await http.post(
-      Uri.parse(
-        'http://192.168.100.174:34603/api/Security/UserCanLoginPlain?user=$user&password=$password',
-      ),
-    );
-
-    var responseOfUser = json.decode(response.body);
-    String responseInString = responseOfUser.toString();
-    loginMessage = responseInString;
-
-    //pega o retorno do login e coloca na variável "loginMessage" pra ter acesso a mensagem na tela de autenticação e exibir a mensagem de erro
-    errorMessage(loginMessage);
+    String error = '';
+    try {
+      final response = await http.post(
+        Uri.parse(
+          'http://192.168.100.174:34603/api/Security/UserCanLoginPlain?user=$user&password=$password',
+        ),
+      );
+      var responseOfUser = json.decode(response.body);
+      String responseInString = responseOfUser.toString();
+      loginMessage = responseInString;
+    } catch (e) {
+      print(e);
+      error = e.toString();
+      loginMessage = error;
+      errorMessage(loginMessage);
+    } finally {
+      //pega o retorno do login e coloca na variável "loginMessage" pra ter acesso a mensagem na tela de autenticação e exibir a mensagem de erro
+      errorMessage(error);
+    }
 
     notifyListeners();
   }
@@ -66,30 +76,35 @@ class LoginProvider with ChangeNotifier {
       _enterprises.clear();
     }
 
+    try {
     var headers = {'Content-Type': 'application/json'};
-    var request = http.Request(
-        'POST',
-        Uri.parse(
-            'http://192.168.100.174:34603/api/Enterprise/GetEnterprises'));
-    request.body = json.encode(
-        "¬EncryptedDataInfo£¬EncryptedData£zFU/ChZcnVrG7+GL3Jw2t+9MFhKc3XAzUN+jwgNNLtvcDOcqWy71Wh/+5/TE2wh5gJrVI7wiUVsybH/z9aI6yq3tWJAn2vklx9ylc/HceLfQJ1fBhXgfHLnOuP1dxls/xOPwHxo4GL8MXPzpGvEoQbAlLNrzU5WzcDGrOgv0yNijAJcwIpvivgYlvGKl0zGXQqpn3Ctx9X4TB4YRV7tUzeaYHxI4ZhKwzKXJxAM9fM2t6RxmE4NdIidygj/Cp7mgJZ6N1HNH6rlwYyLYcPcupNkbVgaQs/z/y1/qikUV+VUJydUYmw48G+PwHd2WMmLs9h1l7rLuxXclU5DT61bHlHScsEQanIUviX0BbhMlAXsmdYCDwG1yAg6dk8DsIHR0p1B5Pv6kS8F0elywus2GXnRXNs+w3K8X¬/EncryptedData£¬EncryptedIV£UsgMfHU9ABHKJqtyt9Nr8pXsRFQI20KHdjTXG4bKQ9kOv9/rSIoiq5m6JqgMTGOeIzm3Fm6sAUTtlGuGFiUJS2bK91M6yKWXwsyW0vgHzdk7AWs+9Rw0YOMJ6FYUE1ENLzfXm7gQJIyxuUy4xdeHRNWBHVk19b00mWDD2xQfkxA=¬/EncryptedIV£¬EncryptedKey£nyPCcup6vNvc3cGq5+FFQG0QbM6j3lFChm1R/Ko0pWriWOqV15JeeO+CYd/CfAjr1PE8HdwTk0uMqVqirUFeZLXuvsM9d6WQATuPddfw8PctmQ73alwOvkjPDkKYS9wrRfijHRTSZ85ESCDjmqouW91Hrmi5MUpwkqwO0bZ0N1o=¬/EncryptedKey£¬/EncryptedDataInfo£");
-    request.headers.addAll(headers);
+      var request = http.Request(
+          'POST',
+          Uri.parse(
+              'http://192.168.100.174:34603/api/Enterprise/GetEnterprises'));
+      request.body = json.encode(
+          "¬EncryptedDataInfo£¬EncryptedData£zFU/ChZcnVrG7+GL3Jw2t+9MFhKc3XAzUN+jwgNNLtvcDOcqWy71Wh/+5/TE2wh5gJrVI7wiUVsybH/z9aI6yq3tWJAn2vklx9ylc/HceLfQJ1fBhXgfHLnOuP1dxls/xOPwHxo4GL8MXPzpGvEoQbAlLNrzU5WzcDGrOgv0yNijAJcwIpvivgYlvGKl0zGXQqpn3Ctx9X4TB4YRV7tUzeaYHxI4ZhKwzKXJxAM9fM2t6RxmE4NdIidygj/Cp7mgJZ6N1HNH6rlwYyLYcPcupNkbVgaQs/z/y1/qikUV+VUJydUYmw48G+PwHd2WMmLs9h1l7rLuxXclU5DT61bHlHScsEQanIUviX0BbhMlAXsmdYCDwG1yAg6dk8DsIHR0p1B5Pv6kS8F0elywus2GXnRXNs+w3K8X¬/EncryptedData£¬EncryptedIV£UsgMfHU9ABHKJqtyt9Nr8pXsRFQI20KHdjTXG4bKQ9kOv9/rSIoiq5m6JqgMTGOeIzm3Fm6sAUTtlGuGFiUJS2bK91M6yKWXwsyW0vgHzdk7AWs+9Rw0YOMJ6FYUE1ENLzfXm7gQJIyxuUy4xdeHRNWBHVk19b00mWDD2xQfkxA=¬/EncryptedIV£¬EncryptedKey£nyPCcup6vNvc3cGq5+FFQG0QbM6j3lFChm1R/Ko0pWriWOqV15JeeO+CYd/CfAjr1PE8HdwTk0uMqVqirUFeZLXuvsM9d6WQATuPddfw8PctmQ73alwOvkjPDkKYS9wrRfijHRTSZ85ESCDjmqouW91Hrmi5MUpwkqwO0bZ0N1o=¬/EncryptedKey£¬/EncryptedDataInfo£");
+      request.headers.addAll(headers);
 
-    http.StreamedResponse response = await request.send();
-    String resultAsString = await response.stream.bytesToString();
-    List resultAsList = json.decode(resultAsString);
-    Map resultAsMap = resultAsList.asMap();
+      http.StreamedResponse response = await request.send();
+      String resultAsString = await response.stream.bytesToString();
+      List resultAsList = json.decode(resultAsString);
+      Map resultAsMap = resultAsList.asMap();
 
-    resultAsMap.forEach((id, data) {
-      _enterprises.add(
-        Enterprise(
-          codigoInternoEmpresa: data['CodigoInterno_Empresa'],
-          codigoEmpresa: data['Codigo_Empresa'],
-          nomeEmpresa: data['Nome_Empresa'],
-          isMarked: false,
-        ),
-      );
-    });
+      resultAsMap.forEach((id, data) {
+        _enterprises.add(
+          Enterprise(
+            codigoInternoEmpresa: data['CodigoInterno_Empresa'],
+            codigoEmpresa: data['Codigo_Empresa'],
+            nomeEmpresa: data['Nome_Empresa'],
+            isMarked: false,
+          ),
+        );
+      });
+    } catch (e) {
+      e;
+      print(e.toString());
+    } finally {}
 
     notifyListeners();
   }
